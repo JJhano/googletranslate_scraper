e425d0a008089285de871f4dd25f9217ae150ba2
JJhano <jhano@DESKTOP-QOOMGA9.localdomain>
2024-01-27 03:29:21 -0300

    extract deck file

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 import sys
 from dotenv import load_dotenv
 
+example_html_to_add = "<br>[sound:Leverage.mp3]"
+BR = "<br>"
+
 def printWarning(message):
     message = f'WARNING: {message}'
     print('=' * len(message))
@@ -45,8 +48,69 @@ def getWordsFromFile(file_path):
     except Exception as e:
         print(f"ERROR: {e}")
         exit()
+# ankideck .txt to array format
+def transformFile(file_path):
+    try:
+        array = []
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            lines = lines[2:]
+            for i in range(0, len(lines), 2):
+                word, _ = lines[i].split('\t')
+                if not word.__contains__(BR):
+                    array.append(word)
+            return array
 
+    except FileNotFoundError:
+        print(f"ERROR: File' {file_path}' not found")
+        exit()
+    except Exception as e:
+        print(f"ERROR: {e}")
+        exit()
+
+
+
+def createTxtFile(array, nombre_archivo):
+    try:
+        with open(nombre_archivo, 'w') as archivo:
+            for elemento in array:
+                archivo.write(str(elemento) + '\n')
+        print(f'Se ha creado el archivo "{nombre_archivo}" con éxito.')
+    except Exception as e:
+        print(f'Ocurrió un error al crear el archivo: {e}')
+
+def endPosSubstring(cadena, substring):
+    indice = cadena.rfind(substring)
+    if indice != -1:
+        end_pos = indice + len(substring) - 1
+        return end_pos
+    else:
+        return -1
+    
+def addAudioToAnkiTxt(file_path, words):
+    try:
+        num = len(words)
+        cont = 0
+        with open(file_path, 'wr') as file:
+            lines = file.readlines()
+            for line in lines:
+                if(not line.__contains__(BR)):
+                    while (cont < num and not line.__contains__(words[cont])):
+                        cont += 1
+                    
+                cont = 0
+
+    except FileNotFoundError:
+        print(f"ERROR: File' {file_path}' not found")
+        exit()
+    except Exception as e:
+        print(f"ERROR: {e}")
+        exit()
+
+
+    
 def main():
+
     load_dotenv('.env')
     URL = os.environ.get('TRANSLATE_URL')
     DIR = os.environ.get('DIR')
@@ -71,7 +135,11 @@ def main():
         exit()
     else:
         using_file = True
-
+    array = transformFile("English words.txt")
+    # # print(endPosSubstring("hola co ", "co"))
+    # exit()
+    createTxtFile(array, "words.txt")
+    # exit()
     workdir = os.getcwd()
     print(f'Working directory: {workdir}')
     if not os.path.exists(DIR):
