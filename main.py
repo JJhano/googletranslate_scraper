780d0593ad16116cbbb7d9e026d8cd1f3aaf4856
JJhano <jhano@DESKTOP-QOOMGA9.localdomain>
2024-01-27 22:52:48 -0300

    Funcionando, falta corregir manejo substring

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import requests as req
 import os
 import sys
 from dotenv import load_dotenv
+import shutil 
 
 example_html_to_add = "<br>[sound:Leverage.mp3]"
 BR = "<br>"
 html_to_add = '<br>[sound:'
+
 def printWarning(message):
     message = f'WARNING: {message}'
     print('=' * len(message))
@@ -15,6 +17,7 @@ def printWarning(message):
 def getAudio(url, filename):
     if(os.path.exists(filename)):
         printWarning(f'File {filename} already exists')
+        return
         # exit()
     try:
         print(f'Request HTTP: {url}')
@@ -32,13 +35,13 @@ def getAudio(url, filename):
 def getExtension(filename):
     _, extension = os.path.splitext(filename)
     return extension
-
+#Get the words from txt file with only words
 def getWordsFromFile(file_path):
     try:
         with open(file_path, 'r') as file:
             lines = file.readlines()
             words = [line.replace('\n','') for line in lines]
-            # print(words)
+            
         return words
 
     except FileNotFoundError:
@@ -53,72 +56,72 @@ def transformFile(file_path):
         array = []
         with open(file_path, 'r') as file:
             lines = file.readlines()
-            lines = lines[2:]
-            for i in range(0, len(lines), 2):
-                word, _ = lines[i].split('\t')
+            new_lines = lines[2:]
+            for i in range(0, len(new_lines), 2):
+                word, _ = new_lines[i].split('\t')
                 if not word.__contains__(BR):
                     array.append(word)
-        return array
+        return array, lines
 
     except FileNotFoundError:
         print(f"ERROR: File' {file_path}' not found")
-        exit()
     except Exception as e:
         print(f"ERROR: {e}")
-        exit()
 
 
-
-def createTxtFile(array, nombre_archivo):
+def createTxtFile(array, nombre_archivo, salto_linea='\n'):
     try:
         with open(nombre_archivo, 'w') as archivo:
             for elemento in array:
-                archivo.write(str(elemento) + '\n')
+                archivo.write(str(elemento) + salto_linea)
         print(f'Se ha creado el archivo "{nombre_archivo}" con éxito.')
     except Exception as e:
         print(f'Ocurrió un error al crear el archivo: {e}')
 
 def endPosSubstring(cadena, substring):
-    indice = cadena.rfind(substring)
+    indice = cadena.find(substring)
     if indice != -1:
         end_pos = indice + len(substring) - 1
         return end_pos
     else:
         return -1
     
-def addAudioToAnkiTxt(file_path, words):
-    global EXT
-    try:
-        num = len(words)
+def addAudioToAnkiTxt(lines, words, ext):
+    num = len(words)
+    cont = 0
+    array = []
+    for line in lines:
+        if(not line.__contains__(BR)):
+            while (cont < num and not line.__contains__(words[cont])):
+                cont += 1
+            if (cont < num):
+                pos = endPosSubstring(line, words[cont])
+                subs = line[:pos + 1] + html_to_add + words[cont].lower() + ext + ']'
+                subs += line[pos + 1: len(line) + 1]
+                array.append(subs)
+        else:
+            array.append(line)
         cont = 0
-        with open(file_path, 'wr') as file:
-            lines = file.readlines()
-            array = []
-            for line in lines:
-                if(not line.__contains__(BR)):
-                    while (cont < num and not line.__contains__(words[cont])):
-                        cont += 1
-                    if (cont < num):
-                        pos = endPosSubstring(line, words[cont])
-                        subs = line[:pos + 1] + html_to_add + words[cont] + EXT + ']'
-                        subs += line[pos + 1: len(line) + 1]
-                        array.append(subs)
-            cont = 0
-        print(array)
+    return array
 
-    except FileNotFoundError:
-        print(f"ERROR: File' {file_path}' not found")
-        exit()
-    except Exception as e:
-        print(f"ERROR: {e}")
-        exit()
-
+def moveToCollection(directory_collection, files):
+    print(f'Colection directory %s', directory_collection)
+    print(f'Files %s', files)
+    for file in files:
+        try:
+            shutil.move(file, directory_collection)
+            print(f'File {file} moved' )
+        except Exception as e:
+            print(f'ERROR: {file}')
+            print(e)
+    print(f'[+] Files moved')
+    
 def main():
-
     load_dotenv('.env')
     URL = os.environ.get('TRANSLATE_URL')
     DIR = os.environ.get('DIR')
     EXT = os.environ.get('EXT')
+    ANKI_COLLECTION_DIRECTORY = os.environ.get('ANKI_COLLECTION_DIRECTORY')
     filename = os.environ.get('WORDS_FILE')
     if (URL == None or DIR == None or EXT == None):
         print("ERROR can't import environment variables")
@@ -139,10 +142,10 @@ def main():
         exit()
     else:
         using_file = True
-    array = transformFile("English words.txt")
+    array, lines = transformFile("English words.txt")
     # # print(endPosSubstring("hola co ", "co"))
     # exit()
-    createTxtFile(array, "words.txt")
+    createTxtFile(array, "words.txt", '\n')
     # exit()
     workdir = os.getcwd()
     print(f'Working directory: {workdir}')
@@ -156,12 +159,18 @@ def main():
             for word in words:
                 word = word.lower()
                 getAudio(f'{URL}{word}', f'{DIR}/{word}{EXT}')
-            # addAudioToAnkiTxt('Exglish words.txt', words)
             
         else:
             word = word.lower()
             getAudio(f'{URL}{word}', f'{DIR}/{word}{EXT}')
     else:
         printWarning(f'No URL specified')
+    lines = addAudioToAnkiTxt(lines, words, EXT)
+    createTxtFile(lines, "new english words.txt", '')
+    files =  os.listdir(DIR)
+    os.chdir(DIR)
+    moveToCollection(ANKI_COLLECTION_DIRECTORY,files)
     print('[+] Done')
-main()
+
+if __name__ == '__main__':
+    main()
